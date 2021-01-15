2403f5cf10d010b007accca64297bf06266d8d36
Batr <web.dev.fil@yandex.ru>
2021-01-15 19:57:51 +0800

    travel

--- app/js/main.js
+++ app/js/main.js
@@ -28,4 +28,11 @@ $(function () {
         focusOnSelect: true,
         asNavFor: '.surf-slider',
     });
+
+    $('.travel__slider').slick({
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>'
+    });
 });
